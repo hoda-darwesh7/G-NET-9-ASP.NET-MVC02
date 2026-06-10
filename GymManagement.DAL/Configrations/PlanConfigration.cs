b894ca915547c7542fb0edf1499c7d8fe55bfe87
Mahmoud Abdel Sami <Mahmoud.Abdel.Sami113@science.helwan.edu.eg>
2026-06-10 05:53:14 +0300

    PL&BLL&DAL

---
+++ GymManagement.DAL/Configrations/PlanConfigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using GymManagement.Models;
+
+namespace GymManagement.Configrations
+{
+    public class PlanConfigration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(x => x.Name).HasColumnType("varchar(30)");
+
+            builder.Property(d => d.Description).HasMaxLength(200);
+
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.Property(c => c.CreatedAt).HasDefaultValueSql("GETDATE()");
+
+            builder.Property(U=>U.UpdatedAt).HasDefaultValueSql("GETDATE()");
+
+            builder.ToTable(TB =>
+            {
+                TB.HasCheckConstraint("PlanDurationCheck", "DurationDays Between 1 and 365");
+            });
+        }
+
+    }
+}
---
+++ GymManagement.DAL/Context/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GymManagement.Configrations;
+using GymManagement.Models;
+using System.Numerics;
+
+namespace GymManagement.Context
+{
+    public class GymDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = 7ODA ; Database = GymDb2 ; Trusted_Connection = true ; TrustServerCertificate = true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfigration());
+        }
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
